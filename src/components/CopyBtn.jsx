ef52bf4e46ed724bdf95f0e467125775d58450c4
Lunox <lunox@Lunoxs-MacBook-Pro.local>
2023-12-05 00:20:08 -0400

    SEO added

--- src/components/CopyBtn.jsx
+++ src/components/CopyBtn.jsx
@@ -4,14 +4,15 @@ const CopyBtn = ({ textCopied }) => {
   const [copied, setCopied] = useState(false);
 
   const handleCopyText = useCallback(() => {
-    navigator.clipboard.writeText(textCopied)
+    navigator.clipboard
+      .writeText(textCopied)
       .then(() => {
         setCopied(true);
         setTimeout(() => {
           setCopied(false);
         }, 2000);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error al copiar", error);
       });
   }, [textCopied]);
@@ -24,25 +25,20 @@ const CopyBtn = ({ textCopied }) => {
         aria-label="Copiar texto"
         className="inline-flex justify-center items-center p-2 text-white rounded cursor-pointer hover:bg-gray-600"
       >
-        {copied && 
-        <span className="absolute mr-36 text-xs whitespace-nowrap hover:text-n">
-          ¡Texto copiado!
-        </span>}
-        <svg
-          width="16"
-          height="16"
-          viewBox="0 0 256 256"
-        >
+        {copied && (
+          <span className="absolute mr-36 text-xs whitespace-nowrap hover:text-n">
+            ¡Texto copiado!
+          </span>
+        )}
+        <svg width="20" height="20" viewBox="0 0 256 256">
           <g fill="#ffffff">
             <path d="M216 40v128h-48V88H88V40Z" opacity=".2"></path>
-            <path
-              d="M216 32H88a8 8 0 0 0-8 8v40H40a8 8 0 0 0-8 8v128a8 8 0 0 0 8 8h128a8 8 0 0 0 8-8v-40h40a8 8 0 0 0 8-8V40a8 8 0 0 0-8-8Zm-56 176H48V96h112Zm48-48h-32V88a8 8 0 0 0-8-8H96V48h112Z"
-            ></path>
+            <path d="M216 32H88a8 8 0 0 0-8 8v40H40a8 8 0 0 0-8 8v128a8 8 0 0 0 8 8h128a8 8 0 0 0 8-8v-40h40a8 8 0 0 0 8-8V40a8 8 0 0 0-8-8Zm-56 176H48V96h112Zm48-48h-32V88a8 8 0 0 0-8-8H96V48h112Z"></path>
           </g>
         </svg>
       </button>
     </>
   );
-}
+};
 
 export default CopyBtn;
