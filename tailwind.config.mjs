6bc8e2853e2ea950e8e28ce4deb69142a6fad313
lunoxdev <lunox.code@gmail.com>
2024-01-18 00:28:04 -0400

    Se actualiza Tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,36 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			"animation": {
+				"background-shine": "background-shine 2s linear infinite",
+			  },
+			  "keyframes": {
+				"background-shine": {
+				  "from": {
+					"backgroundPosition": "0 0"
+				  },
+				  "to": {
+					"backgroundPosition": "-200% 0"
+				  }
+				}
+			},
+			"animation": {
+				"border-width": "border-width 3s infinite alternate"
+			  },
+			  "keyframes": {
+				"border-width": {
+				  "from": {
+					"width": "10px",
+					"opacity": "0"
+				  },
+				  "to": {
+					"width": "100px",
+					"opacity": "1"
+				  }
+				}
+			  }
+		},
 	},
 	plugins: [],
 }
